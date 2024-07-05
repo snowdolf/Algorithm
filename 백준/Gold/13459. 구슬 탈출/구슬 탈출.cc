94ade9a5ba4082d2ffc57bf3bec8397e78c90383
snowdolf <156879693+snowdolf@users.noreply.github.com>
2024-07-05 22:54:36 +0900

    [Gold I] Title: 구슬 탈출, Time: 0 ms, Memory: 2552 KB -BaekjoonHub

--- 백준/Gold/13459. 구슬 탈출/구슬 탈출.cc
+++ 백준/Gold/13459. 구슬 탈출/구슬 탈출.cc
@@ -3,11 +3,10 @@
 
 using namespace std;
 
-struct cell
+struct Cell
 {
     int cnt, rx, ry, bx, by;
 };
-
 
 int n, m, rx, ry, bx, by;
 char board[15][15];
@@ -15,9 +14,41 @@ string str;
 
 int dx[4] = {-1, 1, 0, 0};
 int dy[4] = {0, 0, -1, 1};
-bool isRedFirst, isRedGoal, isBlueGoal;
 
-queue<cell> que;
+queue<Cell> que;
+
+bool move(int &x, int &y, int dir, bool &goal)
+{
+    while (true)
+    {
+        x += dx[dir];
+        y += dy[dir];
+        if (board[x][y] == 'O')
+        {
+            goal = true;
+            return true;
+        }
+        if (board[x][y] == '#')
+        {
+            x -= dx[dir];
+            y -= dy[dir];
+            return false;
+        }
+    }
+}
+
+bool isPriorRed(int rx, int ry, int bx, int by, int dir)
+{
+    if (dir == 0 && ry == by && rx < bx)
+        return true;
+    if (dir == 1 && ry == by && rx > bx)
+        return true;
+    if (dir == 2 && rx == bx && ry < by)
+        return true;
+    if (dir == 3 && rx == bx && ry > by)
+        return true;
+    return false;
+}
 
 int main()
 {
@@ -25,97 +56,69 @@ int main()
     cin.tie(0);
 
     cin >> n >> m;
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         cin >> str;
-        for(int j = 0; j < m; j++){
+        for (int j = 0; j < m; j++)
+        {
             board[i][j] = str[j];
-            if(board[i][j] == 'R')rx = i, ry = j, board[i][j] = '.';
-            if(board[i][j] == 'B')bx = i, by = j, board[i][j] = '.';
+            if (board[i][j] == 'R')
+                rx = i, ry = j, board[i][j] = '.';
+            if (board[i][j] == 'B')
+                bx = i, by = j, board[i][j] = '.';
         }
     }
 
     que.push({0, rx, ry, bx, by});
-    while(!que.empty()){
-        cell qf = que.front();
+    while (!que.empty())
+    {
+        Cell qf = que.front();
         que.pop();
 
-        if(qf.cnt == 10)break;
-        for(int i = 0; i < 4; i++){
-            isRedFirst = false;
-            isRedGoal = false;
-            isBlueGoal = false;
+        if (qf.cnt == 10)
+            break;
 
-            if(i == 0 && qf.ry == qf.by && qf.rx < qf.bx)isRedFirst = true;
-            if(i == 1 && qf.ry == qf.by && qf.rx > qf.bx)isRedFirst = true;
-            if(i == 2 && qf.rx == qf.bx && qf.ry < qf.by)isRedFirst = true;
-            if(i == 3 && qf.rx == qf.bx && qf.ry > qf.by)isRedFirst = true;
+        for (int i = 0; i < 4; i++)
+        {
+            bool isRedFirst = isPriorRed(qf.rx, qf.ry, qf.bx, qf.by, i);
+            bool isRedGoal = false, isBlueGoal = false;
 
             int nrx = qf.rx, nry = qf.ry, nbx = qf.bx, nby = qf.by;
 
-            if(isRedFirst){
-                while(!isRedGoal){
-                    nrx += dx[i];
-                    nry += dy[i];
-                    if(board[nrx][nry] == 'O'){
-                        isRedGoal = true;
-                    }
-                    else if(board[nrx][nry] == '#'){
-                        nrx -= dx[i];
-                        nry -= dy[i];
-                        break;
-                    }
-                }
-                while(!isBlueGoal){
-                    nbx += dx[i];
-                    nby += dy[i];
-                    if(board[nbx][nby] == 'O'){
-                        isBlueGoal = true;
-                    }
-                    else if(board[nbx][nby] == '#' || (nrx == nbx && nry == nby)){
-                        nbx -= dx[i];
-                        nby -= dy[i];
-                        break;
-                    }
+            if (isRedFirst)
+            {
+                move(nrx, nry, i, isRedGoal);
+                move(nbx, nby, i, isBlueGoal);
+                if (nrx == nbx && nry == nby && !isBlueGoal)
+                {
+                    nbx -= dx[i];
+                    nby -= dy[i];
                 }
             }
-            else{
-                while(!isBlueGoal){
-                    nbx += dx[i];
-                    nby += dy[i];
-                    if(board[nbx][nby] == 'O'){
-                        isBlueGoal = true;
-                    }
-                    else if(board[nbx][nby] == '#'){
-                        nbx -= dx[i];
-                        nby -= dy[i];
-                        break;
-                    }
-                }
-                while(!isRedGoal){
-                    nrx += dx[i];
-                    nry += dy[i];
-                    if(board[nrx][nry] == 'O'){
-                        isRedGoal = true;
-                    }
-                    else if(board[nrx][nry] == '#' || (nrx == nbx && nry == nby)){
-                        nrx -= dx[i];
-                        nry -= dy[i];
-                        break;
-                    }
+            else
+            {
+                move(nbx, nby, i, isBlueGoal);
+                move(nrx, nry, i, isRedGoal);
+                if (nrx == nbx && nry == nby && !isRedGoal)
+                {
+                    nrx -= dx[i];
+                    nry -= dy[i];
                 }
             }
 
-            if(qf.rx == nrx && qf.ry == nry && qf.bx == nbx && qf.by == nby)continue;
-
-            if(isRedGoal && !isBlueGoal){
+            if (isRedGoal && !isBlueGoal)
+            {
                 cout << "1";
                 return 0;
             }
-            else if(!isBlueGoal)que.push({qf.cnt + 1, nrx, nry, nbx, nby});
+
+            if (!isBlueGoal && (qf.rx != nrx || qf.ry != nry || qf.bx != nbx || qf.by != nby))
+            {
+                que.push({qf.cnt + 1, nrx, nry, nbx, nby});
+            }
         }
     }
 
     cout << "0";
-
     return 0;
 }

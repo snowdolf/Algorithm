d968abb982062e2550fed43cbce946282915cbf9
snowdolf <156879693+snowdolf@users.noreply.github.com>
2024-07-20 13:13:43 +0900

    [Bronze IV] Title: Next in line, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int a, b;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> a >> b;
+
+    cout << 2 * b - a;
+
+    return 0;
+}

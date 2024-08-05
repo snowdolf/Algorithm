35f9b49958d2b659a7d3d8551280a0c69a7ae661
snowdolf <156879693+snowdolf@users.noreply.github.com>
2024-08-05 11:16:42 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 1740 ms, Memory: 57124 KB -BaekjoonHub

---
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <queue>
+#include <map>
+
+using namespace std;
+
+int t, k, n;
+char q;
+
+priority_queue<int> maxpq;
+priority_queue<int, vector<int>, greater<int>> minpq;
+map<int, int> cntmp;
+
+void init()
+{
+    while (!maxpq.empty())
+        maxpq.pop();
+    while (!minpq.empty())
+        minpq.pop();
+    cntmp.clear();
+    return;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> t;
+    while (t--)
+    {
+        cin >> k;
+
+        init();
+
+        while (k--)
+        {
+            cin >> q >> n;
+            if (q == 'I')
+            {
+                maxpq.push(n);
+                minpq.push(n);
+                cntmp[n]++;
+            }
+            else if (n == 1)
+            {
+                if (!maxpq.empty())
+                {
+                    cntmp[maxpq.top()]--;
+                    maxpq.pop();
+                }
+            }
+            else if (n == -1)
+            {
+                if (!minpq.empty())
+                {
+                    cntmp[minpq.top()]--;
+                    minpq.pop();
+                }
+            }
+            while (!maxpq.empty() && !cntmp[maxpq.top()])
+                    maxpq.pop();
+            while (!minpq.empty() && !cntmp[minpq.top()])
+                    minpq.pop();
+        }
+
+        if (maxpq.empty() || minpq.empty())
+            cout << "EMPTY\n";
+        else
+            cout << maxpq.top() << " " << minpq.top() << "\n";
+    }
+
+    return 0;
+}

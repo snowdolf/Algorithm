284e7204fa617773e03c760be102c2f065b9b5a0
snowdolf <156879693+snowdolf@users.noreply.github.com>
2024-08-13 11:30:38 +0900

    [Silver I] Title: IOIOI, Time: 16 ms, Memory: 13868 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.cc
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -1,26 +1,34 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
+
+int n, m, ans, kmp[2000010];
+string a, b;
 
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int n, m, ans;
-    string a, b, c;
+    cin >> n >> m >> a;
 
-    cin >> n >> m >> b;
-
-    a = "I";
-    for(int i = 0; i < n; i++)a += "OI";
+    b = "I";
+    for(int i = 0; i < n; i++)b += "OI";
 
     n = 2 * n + 1;
 
-    for(int i = 0; i <= m - n; i++){
-        c = b.substr(i, n);
-        if(a == c)ans++;
+    for(int i = 1, j = 0; i < n; i++){
+        while(j && b[i] != b[j])j = kmp[j - 1];
+        if(b[i] == b[j])kmp[i] = ++j;
+    }
+
+    for(int i = 0, j = 0; i < m; i++){
+        while(j && a[i] != b[j])j = kmp[j - 1];
+        if(a[i] == b[j])j++;
+        if(j == n){
+            ans++;
+            j = kmp[j - 1];
+        }
     }
 
     cout << ans;

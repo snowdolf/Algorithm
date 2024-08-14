712a0231dad749de475ed0a3e794b5f46ed54bfb
snowdolf <156879693+snowdolf@users.noreply.github.com>
2024-08-14 09:55:02 +0900

    [Silver I] Title: 카잉 달력, Time: 96 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t, m, n, x, y, ans;
+
+    cin >> t;
+    while(t--){
+        cin >> m >> n >> x >> y;
+        ans = -1;
+        for(int i = 0; i < n; i++){
+            int cnt = m * i + x;
+            y %= n;
+            if(cnt % n == y){
+                ans = cnt;
+                break;
+            }
+        }
+        cout << ans << "\n";
+    }
+
+    return 0;
+}

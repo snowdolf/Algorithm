2db2ef357d02d612378673ef79d09caee5ba2842
snowdolf <156879693+snowdolf@users.noreply.github.com>
2024-09-26 14:06:44 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 68 ms, Memory: 2996 KB -BaekjoonHub

---
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 1000000;
+
+int n;
+bool chk[MAX + 1];
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    for (int i = 2; i <= MAX; i++)
+    {
+        if (chk[i])
+            continue;
+        for (int j = 2 * i; j <= MAX; j += i)
+        {
+            chk[j] = true;
+        }
+    }
+
+    while (1)
+    {
+        cin >> n;
+        if (!n)
+            break;
+
+        bool cnt = false;
+
+        for (int i = 3; i <= n / 2; i++)
+        {
+            if (!chk[i] && !chk[n - i])
+            {
+                cout << n << " = " << i << " + " << n - i << "\n";
+                cnt = true;
+                break;
+            }
+        }
+
+        if (!cnt)
+            cout << "Goldbach's conjecture is wrong.\n";
+    }
+
+    return 0;
+}

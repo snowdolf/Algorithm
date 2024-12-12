58bbe22eeacc0529967930b8118074125aaaae7e
snowdolf <156879693+snowdolf@users.noreply.github.com>
2024-12-12 16:55:50 +0900

    [Unrated] Title: 엔드게임 스포일러, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Unrated/17295. 엔드게임 스포일러/엔드게임 스포일러.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string str;
+    cin >> str;
+
+    cout << "Avengers: Endgame";
+
+    return 0;
+}

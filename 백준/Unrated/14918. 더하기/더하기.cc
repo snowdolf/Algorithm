29a1452654f7439511fe5ead523d36ae2dbb295b
snowdolf <156879693+snowdolf@users.noreply.github.com>
2025-01-13 12:55:56 +0900

    [Unrated] Title: 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Unrated/14918. 더하기/더하기.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int x, y;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> x >> y;
+    cout << x + y << "\n";
+
+    return 0;
+}

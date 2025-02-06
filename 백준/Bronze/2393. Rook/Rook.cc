7aa50f208c482257eb6cec18f7e7527cdf50a234
snowdolf <156879693+snowdolf@users.noreply.github.com>
2025-02-06 12:38:43 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cout << "  ___  ___  ___" << "\n";
+    cout << "  | |__| |__| |" << "\n";
+    cout << "  |           |" << "\n";
+    cout << "   \\_________/" << "\n";
+    cout << "    \\_______/" << "\n";
+    cout << "     |     |" << "\n";
+    cout << "     |     |" << "\n";
+    cout << "     |     |" << "\n";
+    cout << "     |     |" << "\n";
+    cout << "     |_____|" << "\n";
+    cout << "  __/       \\__" << "\n";
+    cout << " /             \\" << "\n";
+    cout << "/_______________\\" << "\n";
+
+    return 0;
+}

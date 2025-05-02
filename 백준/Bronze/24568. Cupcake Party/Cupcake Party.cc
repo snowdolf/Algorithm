1761ec818df409b716ae3974b6908f5adcf1a9c3
snowdolf <156879693+snowdolf@users.noreply.github.com>
2025-05-02 12:43:00 +0900

    [Bronze V] Title: Cupcake Party, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24568. Cupcake Party/Cupcake Party.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int r, s;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> r >> s;
+
+    cout << 8 * r + 3 * s - 28;
+
+    return 0;
+}

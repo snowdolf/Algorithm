422f54169af90fc424fe68670d55ed18809d5730
snowdolf <156879693+snowdolf@users.noreply.github.com>
2025-05-12 11:34:43 +0900

    [Bronze V] Title: WARBOY, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b, c;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> a >> b >> c;
+
+    cout << 3 * b / a * c;
+
+    return 0;
+}

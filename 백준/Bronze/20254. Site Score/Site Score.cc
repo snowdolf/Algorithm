b834ecaafd71804af5283bc6d451b5a56b2cd70d
snowdolf <156879693+snowdolf@users.noreply.github.com>
2025-05-28 09:50:55 +0900

    [Bronze V] Title: Site Score, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    cout << 56 * a + 24 * b + 14 * c + 6 * d;
+
+    return 0;
+}

4de1073cea0e1bfe0bfbc3db342dedd75e4f7883
snowdolf <156879693+snowdolf@users.noreply.github.com>
2025-06-27 11:37:54 +0900

    [Bronze V] Title: 身長 (Height), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24086. 身長 （Height）/身長 （Height）.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a, b;
+    cin >> a >> b;
+    cout << b - a << endl;
+
+    return 0;
+}

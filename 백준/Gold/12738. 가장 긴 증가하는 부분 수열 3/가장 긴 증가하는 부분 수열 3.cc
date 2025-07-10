789254d0077c40caf68b7e321db01b04bde006c8
snowdolf <156879693+snowdolf@users.noreply.github.com>
2025-07-10 15:46:11 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 168 ms, Memory: 20336 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int &x : a) cin >> x;
+
+    vector<int> lisTail, lisIdx, prevIdx(n, -1);
+    lisTail.push_back(a[0]);
+    lisIdx.push_back(0);
+
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] > lisTail.back())
+        {
+            prevIdx[i] = lisIdx.back();
+            lisTail.push_back(a[i]);
+            lisIdx.push_back(i);
+        }
+        else
+        {
+            auto it = lower_bound(lisTail.begin(), lisTail.end(), a[i]);
+            int idx = it - lisTail.begin();
+            prevIdx[i] = idx > 0 ? lisIdx[idx - 1] : -1;
+            lisTail[idx] = a[i];
+            lisIdx[idx] = i;
+        }
+    }
+
+    cout << lisIdx.size() << "\n";
+
+    return 0;
+}

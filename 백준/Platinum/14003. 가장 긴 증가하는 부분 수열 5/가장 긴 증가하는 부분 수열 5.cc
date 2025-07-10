37e1956090b84d8f892c0c58d0cd8f72c9141bfa
snowdolf <156879693+snowdolf@users.noreply.github.com>
2025-07-10 15:38:29 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 180 ms, Memory: 26356 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -13,37 +13,37 @@ int main()
     cin >> n;
 
     vector<int> a(n);
-    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int &x : a) cin >> x;
 
-    vector<int> b, bIdx, prevIdx(n, 0);
-    b.push_back(a[0]);
-    bIdx.push_back(0);
-    prevIdx[0] = -1;
+    vector<int> lisTail, lisIdx, prevIdx(n, -1);
+    lisTail.push_back(a[0]);
+    lisIdx.push_back(0);
 
     for (int i = 1; i < n; i++)
     {
-        if (a[i] > b.back())
+        if (a[i] > lisTail.back())
         {
-            b.push_back(a[i]);
-            bIdx.push_back(i);
-            prevIdx[i] = bIdx[b.size() - 2];
+            prevIdx[i] = lisIdx.back();
+            lisTail.push_back(a[i]);
+            lisIdx.push_back(i);
         }
         else
         {
-            auto it = lower_bound(b.begin(), b.end(), a[i]);
-            b[it - b.begin()] = a[i];
-            bIdx[it - b.begin()] = i;
-            prevIdx[i] = it - b.begin() > 0 ? bIdx[it - b.begin() - 1] : -1;
+            auto it = lower_bound(lisTail.begin(), lisTail.end(), a[i]);
+            int idx = it - lisTail.begin();
+            prevIdx[i] = idx > 0 ? lisIdx[idx - 1] : -1;
+            lisTail[idx] = a[i];
+            lisIdx[idx] = i;
         }
     }
 
-    cout << b.size() << "\n";
+    cout << lisIdx.size() << "\n";
 
     vector<int> ans;
-    for (int i = bIdx.back(); i != -1; i = prevIdx[i]) ans.push_back(i);
+    for (int i = lisIdx.back(); i != -1; i = prevIdx[i]) ans.push_back(a[i]);
     reverse(ans.begin(), ans.end());
 
-    for (int i : ans) cout << a[i] << " ";
+    for (int i : ans) cout << i << " ";
 
     return 0;
 }

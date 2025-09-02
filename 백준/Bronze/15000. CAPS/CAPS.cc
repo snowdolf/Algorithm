1344396d64f829c3a4bd9876e3bd7522e584840a
snowdolf <156879693+snowdolf@users.noreply.github.com>
2025-09-02 15:27:12 +0900

    [Bronze V] Title: CAPS, Time: 20 ms, Memory: 3680 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string s;
+    cin >> s;
+
+    for (auto c : s)
+    {
+        cout << (char)(c + 'A' - 'a');
+    }
+
+    return 0;
+}

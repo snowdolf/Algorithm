afa150e2c5f1f7b3509c8b0f977cc3babe87de7c
snowdolf <156879693+snowdolf@users.noreply.github.com>
2025-10-28 16:16:58 +0900

    [Bronze III] Title: 3n+1 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14920. 3n＋1 수열/3n＋1 수열.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, t = 1;
+
+    cin >> n;
+    while (n != 1)
+    {
+        if (n % 2 == 0) n /= 2;
+        else n = 3 * n + 1;
+        t++;
+    }
+
+    cout << t;
+
+    return 0;
+} 

a0f9aa4b81fe4a8fe76da9f8bc83e9815eb89410
snowdolf <156879693+snowdolf@users.noreply.github.com>
2025-12-05 15:46:42 +0900

    [Bronze III] Title: 2017 연세대학교 프로그래밍 경시대회, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.cc
+++ 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.cc
@@ -12,15 +12,11 @@ int main()
 
     int ans = 0;
 
-    if (n < 6) ans = 0;
-    else
+    for (int i = 2; i <= n - 4; i += 2)
     {
-        for (int i = 2; i <= n - 4; i += 2)
+        for (int j = 1; j < n - i; j++)
         {
-            for (int j = 1; j < n - i; j++)
-            {
-                if ((j + 2) <= (n - i - j)) ans++;
-            }
+            if ((j + 2) <= (n - i - j)) ans++;
         }
     }
 

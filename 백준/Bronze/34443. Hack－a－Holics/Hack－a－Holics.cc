58598fb7e21695743978043bf748f19aada689b0
snowdolf <156879693+snowdolf@users.noreply.github.com>
2026-01-24 15:54:35 +0900

    [Bronze V] Title: Hack-a-Holics, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34443. Hack－a－Holics/Hack－a－Holics.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, c, p;
+    cin >> n >> c >> p;
+
+    cout << n * p << "\n";
+
+    return 0;
+}
